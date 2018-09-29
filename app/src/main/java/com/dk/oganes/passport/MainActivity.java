8b3846867997ec183dccb5d3c0ae3fca7cf44f26
Oganes Manasian <oganesman8@mail.ru>
2018-09-29 19:37:24 +0300

    Deleted unused activity *deleted display message activity

--- app/src/main/java/com/dk/oganes/passport/MainActivity.java
+++ app/src/main/java/com/dk/oganes/passport/MainActivity.java
@@ -26,15 +26,6 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(stringFromJNI());
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
     /** Ooen Camera activity when special button is pressed */
     public void openCamera(View view) {
         Intent intent = new Intent(this, CameraActivity.class);
