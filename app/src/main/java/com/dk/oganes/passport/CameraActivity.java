030b9ae6a35c98120854d12c74eeaa49b81ed2ec
Oganes Manasian <oganesman8@mail.ru>
2018-09-29 19:29:05 +0300

    Added Camera preview *added CameraPreview Class *added camera preview in layout *added requirements for device *fixed autofocus

--- app/src/main/java/com/dk/oganes/passport/CameraActivity.java
+++ app/src/main/java/com/dk/oganes/passport/CameraActivity.java
@@ -1,14 +1,41 @@
 package com.dk.oganes.passport;
 
+import android.app.Activity;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+
+        preview.addView(mPreview);
+
+    }
 }
